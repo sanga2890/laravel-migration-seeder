489b9825998ca344bb0a77ff1dec4c0cb1b372a7
ste <stefano2890@gmail.com>
2020-07-08 18:30:15 +0200

    also run on linux

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\MAMP\htdocs\boolean\esercizi\laravel-migration-seeder\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\MAMP\htdocs\boolean\esercizi\laravel-migration-seeder\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/boolean/esercizi/laravel-migration-seeder/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/boolean/esercizi/laravel-migration-seeder/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
